ef46f523d030fd7e0b258974190f2b6f9423bf6f
sanya113 <36948791+sanya113@users.noreply.github.com>
2019-04-29 20:19:49 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+RanNum = random.randint(0,1000)
+while true:
+	number = int(input("Напишите число"))
+	if number == RanNum:
+		print("ты угадал")
+		break
+	elif number > RanNum:
+		print("спускай на тренды ютуба")
+	elif number < RanNum:
+		print("больше")
+print("Поздравляю, поздравлений нет!!!")
+input("Нажмите на любую клавишу для продолжения");
