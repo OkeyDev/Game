e86eabbacc62143cb90fa27913057609e93408c4
sanya113 <36948791+sanya113@users.noreply.github.com>
2019-04-29 21:27:10 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 import random
+import os
 RanNum = random.randint(0,1000)
 while True:
 	number = int(input("Напишите число \n"))
 	if number == RanNum:
-		print("ты угадал")
+		os.system("echo -e '\e[1;32mВаu,YouWin!\e[0m'")
 		break
 	elif number > RanNum:
-		print("спускай на тренды ютуба")
+		os.system("echo -e '\e[1;33mСпускайся на тренді ютуба\e[0m'")
 	elif number < RanNum:
-		print("больше")
+		os.system("echo -e '\e[1;34mБольше\e[0m'")
 print("Поздравляю, поздравлений нет!!!")
 input("Нажмите на любую клавишу для продолжения");
