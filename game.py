3b8902d5f57812fec6e371c75abae5290735b719
sanya113 <36948791+sanya113@users.noreply.github.com>
2019-05-02 19:00:41 +0300

    Update game.py

--- game.py
+++ game.py
@@ -3,36 +3,60 @@ import random
 def NormalLevel():
 	randomNum = random.randint(0,1000)
 	while True:
-		number = int(input("Введите колво подписчиков на канале, от 0 до 1000 \n"))
-		if number == randomNum:
-			os.system("echo -e '\e[1;37mПоздравляю, ты угадал\e[0m'")
+		try:
+			number = int(input("Введите колво подписчиков на канале, от 0 до 1000 \n"))
+		except ValueError:
+			os.system("echo -e '\e[31mЯ СКАЗАЛ ВВЕСТИ ЧИСЛО!!!!!\e[0m'")
+		except KeyboardInterrupt:
+			print("\tЛадно, число было " + str(randomNum))
 			break
-		elif number > randomNum:
-			os.system("echo -e '\e[1;33mСпускайся на тренді ютуба'")
+
 		else:
-			os.system("echo -e '\e[1;34mПоднимайся віше гор'")
+			if number == randomNum:
+				os.system("echo -e '\e[1;37mПоздравляю, ты угадал\e[31m'")
+				break
+			elif number > randomNum:
+				os.system("echo -e '\e[1;33mСпускайся на тренді ютуба'")
+			else:
+				os.system("echo -e '\e[1;34mПоднимайся віше гор'")
 def HardLevel():
-	randomNum = random.uniform(0.00,100.00)
+	randomNum = round(random.uniform(0.00,100.00),2)
 	while True:
-		number = float(input("Введите колво подписчиков на канале, от 0.00 до 100.00 \n"))
-		print (randomNum)
-		if number == randomNum:
-			os.system("echo -e '\e[1;37mПоздравляю, ты угадал\e[0m'")
+		try:
+			number = float(input("Введите колво подписчиков на канале, от 0.00 до 100.00 \n"))
+		except ValueError:
+			os.system("echo -e '\e[31mЯ СКАЗАЛ ВВЕСТИ ЧИСЛО!!!!!\e[0m'")
+		except KeyboardInterrupt:
+			print("\tЛадно, число было " + str(randomNum))
 			break
-		elif number > randomNum:
-			os.system("echo -e '\e[1;33mСпускайся на тренді ютуба'")
 		else:
-			os.system("echo -e '\e[1;34mПоднимайся віше гор'")
+			if number == randomNum:
+				os.system("echo -e '\e[1;37mПоздравляю, ты угадал\e[0m'")
+				break
+			elif number > randomNum:
+				os.system("echo -e '\e[1;33mСпускайся на тренді ютуба'")
+			else:
+				os.system("echo -e '\e[1;34mПоднимайся віше гор'")
 
 def VeryHardLevel():
 	while True:
 		randomNum = random.randint(0,100)
-		number = float(input("Введите колво подписчиков на канале, от 0 до 100 \n"))
-		if number == randomNum:
-			os.system("echo -e '\e[1;37mПоздравляю, ты угадал\e[0m'")
+		try:
+			number = float(input("Введите колво подписчиков на канале, от 0 до 100 \n"))
+		except ValueError:
+			os.system("echo -e '\e[31mЯ СКАЗАЛ ВВЕСТИ ЧИСЛО!!!!!\e[0m'")
+		except KeyboardInterrupt:
+			print("\tЛадно, число было " + str(randomNum))
 			break
 		else:
-			os.system("echo -e '\e[1;34mНе угадал,число изменилось'")
+			if number == randomNum:
+				os.system("echo -e '\e[1;37mПоздравляю, ты угадал\e[0m'")
+				break
+			else:
+				os.system("echo -e '\e[1;34mСлучайное число: '",)
+				print(randomNum)
+				os.system("echo -e '\e[0m'")
+				os.system("echo -e '\e[1;34mНе угадал,число изменилось\e[0m'")
 	
 def GetLevel(Level):
 	for i in Level:
@@ -42,11 +66,14 @@ def GetLevel(Level):
 		if Level[i][1] == Choose:
 			Level[i][0]()
 
+
 def StartGameFunc():
-	GetLevel({"Нормально" : [NormalLevel,1],"Сложно(не работае)" : [HardLevel,2],"Экстрим" : [VeryHardLevel,3]})
+	GetLevel({"Нормально" : [NormalLevel,1],"Сложно" : [HardLevel,2],"Экстрим" : [VeryHardLevel,3]})
+
 
 def Exit():
 	print("Нажмите ctrl + c")
+
 
 def Menu(function):
 	while True:
